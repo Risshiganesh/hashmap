d83c949d650759ae71e8f5c564bf319fcdf18801
Risshiganesh Villalan <risshiganesh1997@gmail.com>
2024-01-29 09:20:55 +0800

    No.6 done

---
+++ script.js
@@ -0,0 +1,162 @@
+function checkIndex() {
+  if (index < 0 || index >= buckets.length) {
+    throw new Error("Trying to access index out of bound");
+  }
+}
+
+//1.
+function hashMap() {
+  const buckets = [];
+  buckets.length = 16;
+
+  //2.
+  function hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode % buckets.length;
+  }
+  //3.
+  function set(key, value) {
+    const hashCode = hash(key);
+
+    console.log(hashCode);
+
+    const newNode = {
+      key: key,
+      value: value,
+      next: null,
+    };
+
+    temp = buckets[hashCode];
+
+    //   Add head
+    if (!buckets[hashCode]) {
+      buckets[hashCode] = newNode;
+      console.log(buckets[hashCode]);
+      console.log(buckets);
+      return;
+    }
+
+    //   Check nodes to replace key.
+    while (temp) {
+      if (temp.key === newNode.key) {
+        temp.value = newNode.value;
+        return;
+      }
+
+      // Append node to end of linked list
+      if (!temp.next) {
+        temp.next = newNode;
+        return;
+      }
+
+      temp = temp.next;
+    }
+
+    return;
+  }
+
+  //4.
+  function get(key) {
+    const hashCode = hash(key);
+
+    temp = buckets[hashCode];
+
+    while (temp) {
+      if (temp.key === key) {
+        return temp.value;
+      }
+
+      temp = temp.next;
+    }
+
+    return null;
+  }
+
+  //5.
+  function has(key) {
+    const hashCode = hash(key);
+
+    temp = buckets[hashCode];
+
+    while (temp) {
+      if (temp.key === key) {
+        return true;
+      }
+
+      temp = temp.next;
+    }
+
+    return false;
+  }
+
+  //6.
+  function remove(key) {
+    const hashCode = hash(key);
+
+    temp = buckets[hashCode];
+
+    if (!temp) {
+      return false;
+    }
+
+    if (temp.key === key) {
+      buckets[hashCode] = temp.next;
+      return buckets[hashCode];
+      return true;
+    }
+
+    while (temp.next) {
+      //   console.log("WORKS");
+      if (temp.next.key === key) {
+        temp.next = temp.next.next;
+
+        return buckets[hashCode];
+
+        return true;
+      }
+
+      temp.next = temp.next.next;
+    }
+
+    return false;
+  }
+
+  return {
+    set,
+    get,
+    has,
+    remove,
+  };
+}
+
+const newHashMap = hashMap();
+
+newHashMap.set("Jack", "Doctor");
+
+newHashMap.set("Jqck", "Teacher");
+
+newHashMap.set("Jack", "Surgeon");
+
+newHashMap.set("Jqck", "Lecturer");
+
+console.log(newHashMap.get("Jqck"));
+
+console.log(newHashMap.get("Jack"));
+
+console.log(newHashMap.get("Jok"));
+
+// console.log(newHashMap.has("Jqck"));
+
+// console.log(newHashMap.has("Jack"));
+
+// console.log(newHashMap.has("Jok"));
+
+console.log(newHashMap.remove("Jok"));
+
+console.log(newHashMap.remove("Jqck"));
